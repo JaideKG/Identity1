54f049b75f06ca46f06fd125ec5bad77de95f192
Peter F. Guenther <peter.guenther@gmail.com>
2018-06-05 15:03:30 -0400

    first commit

---
+++ Identity1/Controllers/TasksController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Identity1.Models;
+using Microsoft.AspNet.Identity;
+
+namespace Identity1.Controllers
+{
+    [Authorize]
+    public class TasksController : Controller
+    {
+        private UserTaskEntity db = new UserTaskEntity();
+
+        // GET: Tasks
+        public ActionResult Index()
+        {
+            string uid = User.Identity.GetUserId();
+            return View(
+                (from t in db.Tasks
+                 where t.UserID == uid
+                 select t).ToList()
+                );
+        }
+
+
+        // GET: Tasks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tasks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,UserID,Description,DueDate,Complete")] Tasks tasks)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tasks.Add(tasks);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tasks);
+        }
+
+        // GET: Tasks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tasks tasks = db.Tasks.Find(id);
+            if (tasks == null || tasks.UserID != User.Identity.GetUserId())
+            {
+                return HttpNotFound();
+            }
+            return View(tasks);
+        }
+
+        // POST: Tasks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,UserID,Description,DueDate,Complete")] Tasks tasks)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tasks).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tasks);
+        }
+
+        // GET: Tasks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tasks tasks = db.Tasks.Find(id);
+            if (tasks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tasks);
+        }
+
+        // POST: Tasks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tasks tasks = db.Tasks.Find(id);
+            db.Tasks.Remove(tasks);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
